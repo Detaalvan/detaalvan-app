d2156c7adc484f2e59f48408fd4083f238a1edd9
Detaalvan <info@detaalvan.nl>
2026-03-11 10:03:56 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -189,8 +189,7 @@ export default function App() {
             <DetailPage 
               key="coaching"
               title="Coaching"
-              imageUrl="https://storage.e.jimdo.com/cdn-cgi/image/quality=85,fit=scale-down,format=auto,trim=0;0;0;0,width=925,height=1280/image/543441174/67f7b35e-a5dc-4235-b0c9-012b8584ac1b.png"
-              icon={<Brain />}
+imageUrl="https://storage.e.jimdo.com/cdn-cgi/image/quality=85,fit=scale-down,format=auto,trim=0;0;0;0,width=925,height=1280/image/543441174/67f7b35e-a5dc-4235-b0c9-012b8584ac1b.png"              icon={<Brain />}
               content="Iedereen heeft een autopilot. Niet iedereen ervaart regie. Word bij DeTaalVan meester over je innerlijk dialoog — met hoofd, hart en lijf. Minder reageren, meer kiezen. Zodat je keuzes maakt die écht van jou zijn. Gemiddeld 4 sessies. Vaak vergoed door je werkgever."
               onBack={goBack}
               ctaLink="mailto:info@detaalvan.nl"
